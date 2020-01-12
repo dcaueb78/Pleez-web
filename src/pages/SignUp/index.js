2f74b12701938a6fa943245a7448d1e57af18e20
dcaueb78 <caue1017@hotmail.com>
2020-01-12 19:28:50 -0200

    SignUp schema validation

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
 import logo from '~/assets/logo.png';
 
 export default function SignUp() {
+  const schema = Yup.object().shape({
+    name: Yup.string().required('O nome é obrigatório'),
+    email: Yup.string()
+      .email('Insira um e-mail válido')
+      .required('O e-mail é obrigatório'),
+    password: Yup.string()
+      .required('A senha é obrigatória')
+      .min(6, 'No mínimo 6 caracteres')
+  });
+
   function handleSubmit(data) {
     console.tron.log(data);
   }
@@ -22,7 +33,7 @@ export default function SignUp() {
           </span>
         </div>
 
-        <Form onSubmit={handleSubmit}>
+        <Form schema={schema} onSubmit={handleSubmit}>
           <Input name="name" type="text" placeholder="Seu nome" />
           <Input name="email" type="email" placeholder="Seu e-mail" />
           <Input name="password" type="password" placeholder="Sua senha" />
