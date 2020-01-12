79bc08fd256eef9393bbb17afa1946a4751f56ea
dcaueb78 <caue1017@hotmail.com>
2020-01-12 16:25:17 -0200

    Login styles

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,7 +1,30 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import logo from '~/assets/logo.png';
 
 // import { Container } from './styles';
 
 export default function SignIn() {
-  return <h1>oi</h1>;
+  return (
+    <>
+      <img src={logo} alt="Pleez" />
+
+      <div>
+        <h1>Acesse sua conta</h1>
+        <div>
+          <span>
+            Comece a usar o serviço de pedidos mais rápido que você já viu!
+          </span>
+        </div>
+        <form>
+          <input type="email" placeholder="Seu e-mail" />
+          <input type="password" placeholder="Sua senha" />
+
+          <button type="submit">Acessar</button>
+          <Link to="registro"> Criar conta gratuita </Link>
+        </form>
+      </div>
+    </>
+  );
 }
--- src/pages/_layouts/auth/styles.js
+++ src/pages/_layouts/auth/styles.js
@@ -1,6 +1,88 @@
 import styled from 'styled-components';
+import { darken } from 'polished';
 
 export const Wrapper = styled.div`
   height: 100%;
   background: #ee4162;
+  display: flex;
+  justify-content: center;
+`;
+
+export const Content = styled.div`
+  width: 100%;
+  max-width: 500px;
+  text-align: center;
+  height: 100%;
+  margin: 0 10px;
+
+  img {
+    width: 150px;
+  }
+
+  div {
+    background: #fff;
+    border-radius: 10px;
+    padding-top: 15px;
+
+    h1 {
+      color: #22d6bc;
+      font-size: 34px;
+    }
+
+    div {
+      margin-bottom: 20px;
+      margin-left: 30px;
+      margin-right: 30px;
+
+      span {
+        color: rgba(0, 0, 0, 0.3);
+      }
+    }
+
+    form {
+      display: flex;
+      flex-direction: column;
+      padding: 0 30px;
+
+      input {
+        background: rgba(0, 0, 0, 0.1);
+        border: 0;
+        border-radius: 15px;
+        height: 44px;
+        padding: 0 15px;
+        color: rgba(0, 0, 0, 0.4);
+        margin: 0 0 10px;
+        &::placeholder {
+          color: rgba(0, 0, 0, 0.3);
+        }
+      }
+
+      button {
+        margin: 5px 0 0;
+        height: 44px;
+        background: #22d6bc;
+        font-weight: bold;
+        color: #fff;
+        border: 0;
+        border-radius: 15px;
+        font-size: 16px;
+        transition: background 0.2s;
+
+        &:hover {
+          background: ${darken(0.03, '#22d6bc')};
+        }
+      }
+
+      a {
+        color: #ee4162;
+        font-size: 16px;
+        opacity: 0.8;
+        margin: 10px;
+
+        &:hover {
+          opacity: 1;
+        }
+      }
+    }
+  }
 `;
