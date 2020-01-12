41295dbcb8d96ddc1c12eef34a35800a5bb6629d
dcaueb78 <caue1017@hotmail.com>
2020-01-12 03:30:25 -0200

    Private routes redirect

--- src/pages/Infos/index.js
+++ src/pages/Infos/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 
 // import { Container } from './styles';
 
-export default function Categories() {
-  return <div>Categorias</div>;
+export default function Infos() {
+  return <div />;
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+import Route from './Route';
 
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
@@ -8,6 +9,7 @@ import Categories from '../pages/Categories';
 import Dish from '../pages/Dish';
 import Details from '../pages/Details';
 import Basket from '../pages/Basket';
+import Infos from '../pages/Infos';
 
 export default function Routes() {
   return (
@@ -15,10 +17,13 @@ export default function Routes() {
       <Route path="/" exact component={SignIn} />
       <Route path="/registro" component={SignUp} />
 
-      <Route path="/categorias/:id" exact component={Categories} />
-      <Route path="/pratos/:id/:categoria" exact component={Dish} />
-      <Route path="/detalhes/:id/:prato" exact component={Details} />
-      <Route path="/basket/" exact component={Basket} />
+      <Route path="/categorias/:id" exact component={Categories} isPrivate />
+      <Route path="/pratos/:id/:categoria" exact component={Dish} isPrivate />
+      <Route path="/detalhes/:id/:prato" exact component={Details} isPrivate />
+      <Route path="/basket/" exact component={Basket} isPrivate />
+      <Route path="/informacoes/" exact component={Infos} isPrivate />
+
+      <Route path="/" component={() => <h1>404</h1>} />
     </Switch>
   );
 }
