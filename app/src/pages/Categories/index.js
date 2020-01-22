68bb46c62b7d44244057ebb72c9da9985fdc3102
dcaueb78 <caue1017@hotmail.com>
2020-01-22 20:34:38 -0200

    Cart info styles

--- app/src/pages/Categories/index.js
+++ app/src/pages/Categories/index.js
@@ -5,6 +5,7 @@ import FoodCard from '~/components/FoodCard';
 
 import logo from '~/assets/logo.png';
 import unclejoe from '~/assets/unclejoe.png';
+import cartIcon from '~/assets/icons/CartIcon.png';
 
 export default function Categories() {
   return (
@@ -21,7 +22,18 @@ export default function Categories() {
         </div>
       </Content>
       <div id="cart">
-        testei
+        <div>
+          <h1>0</h1>
+        </div>
+        <div>
+          <div>
+            <span>seta</span>
+            <span>Itens na bandeja</span>
+          </div>
+        </div>
+        <div>
+          <img src={cartIcon} alt="Cart" />
+        </div>
       </div>
     </Wrapper>
   );
--- app/src/pages/Categories/styles.js
+++ app/src/pages/Categories/styles.js
@@ -15,6 +15,26 @@ export const Wrapper = styled.div`
     height: 120px;
     background: #ee4162;
     border-radius: 24px 24px 0 0;
+    display: flex;
+    justify-content: space-around;
+    font-size: 28px;
+    font-weight: 500;
+    align-items: center;
+    padding: 20px;
+    color: #fff;
+
+    div {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      span {
+        font-size: 19px;
+      }
+      img {
+        width: 50px;
+        height: 50px;
+      }
+    }
   }
 `;
 
