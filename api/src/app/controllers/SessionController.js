055cfc669444ff134247c9d38251aa75c0465066
dcaueb78 <caue1017@hotmail.com>
2020-01-24 22:18:44 -0200

    Create SessionController / jwt token

---
+++ api/src/app/controllers/SessionController.js
@@ -0,0 +1,34 @@
+import jwt from 'jsonwebtoken';
+
+import User from '../models/User';
+
+class SessionController {
+  async store(req, res) {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ where: { email } });
+
+    if (!user) {
+      return res.status(401).json({ error: 'User not found' });
+    }
+
+    if (!(await user.checkPassword(password))) {
+      return res.status(401).json({ error: 'Password does not match' });
+    }
+
+    const { id, name } = user;
+
+    return res.json({
+      user: {
+        id,
+        name,
+        email
+      },
+      token: jwt.sign({ id }, '5e55d80da72228344f5dc8ea4914f736', {
+        expiresIn: '7d'
+      })
+    });
+  }
+}
+
+export default new SessionController();
