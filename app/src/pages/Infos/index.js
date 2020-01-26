9d6b49e9915ef03d7912199b1d0221efc4b9b3a1
dcaueb78 <caue1017@hotmail.com>
2020-01-26 21:57:21 -0200

    Create QRcode scanner on info Page

--- app/src/pages/Infos/index.js
+++ app/src/pages/Infos/index.js
@@ -1,7 +1,36 @@
-import React from 'react';
-
-// import { Container } from './styles';
+import React, { useState } from 'react';
+import QrReader from 'react-qr-reader';
 
 export default function Infos() {
-  return <div />;
+  const [result, setResult] = useState('No result');
+  const [delay, setDelay] = useState(1);
+
+  function handleScan(data) {
+    if (data) {
+      setResult(data);
+    }
+  }
+
+  function handleError(err) {
+    console.error(err);
+  }
+
+  const previewStyle = {
+    height: 500,
+    width: 500
+  };
+
+  return (
+    <div>
+      <QrReader
+        resolution={1920}
+        facingMode="environment"
+        delay={delay}
+        style={previewStyle}
+        onError={handleError}
+        onScan={handleScan}
+      />
+      <p>{result}</p>
+    </div>
+  );
 }
