a257c4dd453a0235269a255984d3370398dede7f
dcaueb78 <caue1017@hotmail.com>
2020-01-26 18:52:23 -0200

    Create signUp

--- app/src/pages/SignUp/index.js
+++ app/src/pages/SignUp/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
 import * as Yup from 'yup';
 
 import logo from '~/assets/logo.png';
 
+import { signUpRequest } from '~/store/modules/auth/actions';
+
 export default function SignUp() {
+  const dispatch = useDispatch();
+
   const schema = Yup.object().shape({
     name: Yup.string().required('O nome é obrigatório'),
     email: Yup.string()
@@ -16,8 +21,8 @@ export default function SignUp() {
       .min(6, 'No mínimo 6 caracteres')
   });
 
-  function handleSubmit(data) {
-    console.tron.log(data);
+  function handleSubmit({ name, email, password }) {
+    dispatch(signUpRequest(name, email, password));
   }
 
   return (
--- app/src/store/modules/auth/sagas.js
+++ app/src/store/modules/auth/sagas.js
@@ -32,4 +32,32 @@ export function* signIn({ payload }) {
   }
 }
 
-export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
+export function* signUp({ payload }) {
+  try {
+    const { name, email, password } = payload;
+
+    yield call(api.post, 'users', {
+      name,
+      email,
+      password
+    });
+
+    toast.success('Cadastrado com sucesso :D');
+
+    history.push('/');
+  } catch (err) {
+    toast.error(
+      <div>
+        Houve um problema no cadastro :(
+        <br /> Poderia verificar os seus dados?
+      </div>
+    );
+
+    yield put(signFailure());
+  }
+}
+
+export default all([
+  takeLatest('@auth/SIGN_IN_REQUEST', signIn),
+  takeLatest('@auth/SIGN_UP_REQUEST', signUp)
+]);
