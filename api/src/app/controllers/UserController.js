d7fc76cf8e128cbd262a77251bd42cdb7ba4ac69
dcaueb78 <caue1017@hotmail.com>
2020-01-26 15:04:34 -0200

    Create UPDATE user route api

--- api/src/app/controllers/UserController.js
+++ api/src/app/controllers/UserController.js
@@ -17,9 +17,29 @@ class UserController {
   }
 
   async update(req, res) {
-    console.log(req.userId);
+    const { email, oldPassword } = req.body;
 
-    return res.json({ ok: true });
+    const user = await User.findByPk(req.userId);
+
+    if (email !== user.email) {
+      const userExists = await User.findOne({ where: { email } });
+
+      if (userExists) {
+        return res.status(400).json({ error: 'Email já utilizado!' });
+      }
+    }
+
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({ error: 'As senhas não correspondem!' });
+    }
+
+    const { id, name } = await user.update(req.body);
+
+    return res.json({
+      id,
+      name,
+      email
+    });
   }
 }
 
