f458f5aaddf5bf54e802791ee0e0349b193b30eb
dcaueb78 <caue1017@hotmail.com>
2020-01-28 20:19:14 -0200

    Create profissional account migration

---
+++ api/src/database/migrations/20200128212611-create-profissional-account.js
@@ -0,0 +1,41 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('profissional_account', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+      },
+      cnpj: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      password_hash: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false
+      }
+    });
+  },
+
+  down: queryInterface => {
+    return queryInterface.dropTable('profissional_account');
+  }
+};
