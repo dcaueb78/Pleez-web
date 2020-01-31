a5b84ab3c804ddacc118c034aaab7c0a37fbe82f
dcaueb78 <caue1017@hotmail.com>
2020-01-31 19:37:01 -0200

    Create ProfessionalAccount store route

--- api/src/app/controllers/ProfessionalAccountController.js
+++ api/src/app/controllers/ProfessionalAccountController.js
@@ -1,7 +1,9 @@
 import ProfessionalAccont from '../models/ProfessionalAccount';
 
 class ProfessionalAccountController {
-
+  async store(req, res) {
+    res.json({ ok: 'true' });
+  }
 }
 
 export default new ProfessionalAccountController();
--- api/src/routes.js
+++ api/src/routes.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import ProfessionalAccount from './app/controllers/ProfessionalAccountController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -13,5 +14,7 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
+
+routes.post('/professional-account', ProfessionalAccount.store);
 
 export default routes;
