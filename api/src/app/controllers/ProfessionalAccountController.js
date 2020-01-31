6cf2f1e291d4683db1575faa2770ad8caf228c1c
dcaueb78 <caue1017@hotmail.com>
2020-01-31 19:54:10 -0200

    Create Professional Account store / postgres

--- api/src/app/controllers/ProfessionalAccountController.js
+++ api/src/app/controllers/ProfessionalAccountController.js
@@ -1,8 +1,40 @@
+import * as Yup from 'yup';
 import ProfessionalAccont from '../models/ProfessionalAccount';
 
 class ProfessionalAccountController {
   async store(req, res) {
-    res.json({ ok: 'true' });
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+      password: Yup.string()
+        .required()
+        .min(6),
+      cnpj: Yup.number()
+        .required()
+    });
+
+    if (!(await schema.isValid(req.body))) {
+
+      console.log(req.body);
+      return res.status(400).json({ error: 'Falha de validação!' });
+    }
+    const ProfessionalAccontExists = await ProfessionalAccont.findOne({
+      where: { email: req.body.email }
+    });
+
+    if (ProfessionalAccontExists) {
+      return res.status(400).json({ error: 'Email já cadastrado.' });
+    }
+
+    const { id, name, email } = await ProfessionalAccont.create(req.body);
+
+    return res.json({
+      id,
+      name,
+      email
+    });
   }
 }
 
