256c0fc4464a44de396a4046bb07fce82a8b1d5e
dcaueb78 <caue1017@hotmail.com>
2020-02-03 22:48:15 -0200

    Create categories migration

---
+++ api/src/database/migrations/20200204004417-create-categories.js
@@ -0,0 +1,46 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('categories', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      details: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      price: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false
+      },
+      restaurant_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'restaurants',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+        allowNull: false
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false
+      }
+    });
+  },
+
+  down: queryInterface => {
+    return queryInterface.dropTable('categories');
+  }
+};
