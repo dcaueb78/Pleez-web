e8ce526aad9adbf7ec8e3a74c9c5dcc70c16941b
dcaueb78 <caue1017@hotmail.com>
2020-02-03 22:23:27 -0200

    Create Store Restaurant method

--- api/src/app/controllers/RestaurantController.js
+++ api/src/app/controllers/RestaurantController.js
@@ -1,8 +1,30 @@
+import * as Yup from 'yup';
 import Restaurant from '../models/Restaurant';
 
 class RestaurantController {
   async store(req, res) {
-    res.json({ ok: 'true' });
+    const schema = Yup.object().shape({
+      social_reason: Yup.string().required(),
+      name: Yup.string().required(),
+      telephone: Yup.number().required(),
+      cep: Yup.number().required(),
+      city: Yup.string().required(),
+      state: Yup.string().required(),
+      neighborhood: Yup.string().required(),
+      addres: Yup.string().required(),
+      number: Yup.number(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Falha de validação!' });
+    }
+
+    const { id, name, social_reason } = await Restaurant.create({
+      ...req.body,
+      professional_account_id: req.accountId
+    });
+
+    return res.status(200).json({ id, name, social_reason });
   }
 }
 
