68febbcd24131eb5c9d3c2c48faba015f1963bd0
dcaueb78 <caue1017@hotmail.com>
2020-02-11 14:02:18 -0200

    Update foodcard styles

--- app/src/components/FoodCard/styles.js
+++ app/src/components/FoodCard/styles.js
@@ -5,13 +5,16 @@ export const Container = styled.div`
   margin-left: 15px;
   margin-right: 15px;
   margin-top: 10px;
-  background-color: red;
   height: 78px;
   border-radius: 23px;
   display: flex;
   align-items: center;
   justify-content: center;
   color: white;
+  background-image: url('https://img.elo7.com.br/product/zoom/22565B3/adesivo-parede-prato-comida-frango-salada-restaurante-lindo-adesivo-parede.jpg');
+
+  background-repeat: no-repeat;
+  background-size: 100% 100%;
 
   h1 {
     font-weight: 500;
