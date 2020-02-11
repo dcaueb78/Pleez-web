5eded01fa2b0d2e5bed235afcb29ace5c088ab23
dcaueb78 <caue1017@hotmail.com>
2020-02-11 14:02:01 -0200

    reate Food Card redirect verify and default name

--- app/src/components/FoodCard/index.js
+++ app/src/components/FoodCard/index.js
@@ -3,9 +3,15 @@ import React from 'react';
 import { Container } from './styles';
 import history from '~/services/history';
 
-export default function FoodCard({ name, redirect }) {
+export default function FoodCard({ name= '', redirect }) {
+  function handleRedirect() {
+    if (redirect) {
+      history.push(redirect);
+    }
+  }
+
   return (
-    <Container onClick={() => history.push(redirect)} className="red">
+    <Container onClick={handleRedirect}>
       <h1>{name}</h1>
     </Container>
   );
