f5c231c87da228a31126581432c7530e39d9fe6b
dcaueb78 <caue1017@hotmail.com>
2020-02-11 00:10:07 -0200

    Update foodCard to redirect

--- app/src/components/FoodCard/index.js
+++ app/src/components/FoodCard/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
 import { Container } from './styles';
+import history from '~/services/history';
 
-export default function FoodCard({ name }) {
+export default function FoodCard({ name, redirect }) {
   return (
-    <Container className="red">
+    <Container onClick={() => history.push(redirect)} className="red">
       <h1>{name}</h1>
     </Container>
   );
