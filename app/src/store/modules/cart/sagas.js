3100b0773318e10e485a81c67d756731e1eefacf
dcaueb78 <caue1017@hotmail.com>
2020-02-13 00:55:48 -0200

    Create redirect saga on add to cart

--- app/src/store/modules/cart/sagas.js
+++ app/src/store/modules/cart/sagas.js
@@ -1,3 +1,12 @@
-import { all } from 'redux-saga/effects';
+import React from 'react';
+import { takeLatest, all } from 'redux-saga/effects';
 
-export default all([]);
+import history from '~/services/history';
+
+export function addToCart({ payload }) {
+  if (!payload) return;
+
+  history.goBack(3);
+}
+
+export default all([takeLatest('@cart/ADD_TO_CART', addToCart)]);
