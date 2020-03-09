45ec1fe1cd5bcda2230fdb43a7de048310f9fafc
Cauê Kotarski <caue1017@hotmail.com>
2020-03-08 22:30:16 -0300

    Update restaurant controller to create with recipient

--- api/src/app/controllers/Restaurant/RestaurantController.js
+++ api/src/app/controllers/Restaurant/RestaurantController.js
@@ -1,5 +1,9 @@
 import * as Yup from 'yup';
 import Restaurant from '../../models/Restaurant';
+import ProfessionalAccount from '../../models/ProfessionalAccount';
+
+import pagarme from 'pagarme';
+import { api_key } from '../../../config/pagarme';
 
 class RestaurantController {
   async index(req, res) {
@@ -25,16 +29,50 @@ class RestaurantController {
       state: Yup.string().required(),
       neighborhood: Yup.string().required(),
       addres: Yup.string().required(),
-      number: Yup.number()
+      number: Yup.number(),
+      bank_code: Yup.string().required(),
+      agency: Yup.string().required(),
+      account: Yup.string().required(),
+      account_dv: Yup.string().required()
     });
 
     if (!(await schema.isValid(req.body))) {
       return res.status(400).json({ error: 'Falha de validação!' });
     }
 
-    const { id, name, social_reason } = await Restaurant.create({
+    const { social_reason, bank_code, agency, account, account_dv } = req.body;
+
+    const { cnpj } = await ProfessionalAccount.findByPk(req.accountId);
+
+    let recipient_id = '';
+    await pagarme.client
+      .connect({ api_key })
+      .then(client =>
+        client.recipients.create({
+          transfer_enabled: true,
+          transfer_interval: 'monthly',
+          transfer_day: 5,
+          automatic_anticipation_enabled: false,
+          anticipatable_volume_percentage: 0,
+          bank_account: {
+            bank_code,
+            agencia: agency,
+            conta: account,
+            type: 'conta_corrente',
+            conta_dv: account_dv,
+            document_number: cnpj,
+            legal_name: social_reason
+          }
+        })
+      )
+      .then(recipient => {
+        recipient_id = recipient.id;
+      });
+
+    const { id, name } = await Restaurant.create({
       ...req.body,
-      professional_account_id: req.accountId
+      professional_account_id: req.accountId,
+      recipient_id
     });
 
     return res.status(200).json({ id, name, social_reason });
