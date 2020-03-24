77e5514d43569ca4edfca79a2ef712160236be0f
Cauê Kotarski <caue1017@hotmail.com>
2020-03-23 22:30:35 -0300

    Create global styles

--- restaurant-app/src/routes/index.js
+++ restaurant-app/src/routes/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { Switch } from 'react-router-dom';
 import Route from './Route';
 
-import SignIn from '../pages/SignIn';
-import SignUp from '../pages/SignUp';
+import SignIn from '~/pages/SignIn';
+import SignUp from '~/pages/SignUp';
 
-import Profile from '../pages/Profile';
-import Dashboard from '../pages/Dashboard';
+import Profile from '~/pages/Profile';
+import Dashboard from '~/pages/Dashboard';
 
 export default function Routes() {
   return (
