8a69aa35fca92824c40bbae07ea8a6035dda8eee
Cauê Kotarski <caue1017@hotmail.com>
2020-03-30 21:08:26 -0300

    Create api defaults bearer

--- restaurant-app/src/store/modules/auth/sagas.js
+++ restaurant-app/src/store/modules/auth/sagas.js
@@ -18,6 +18,8 @@ export function* signIn({ payload }) {
 
     const { token, user } = response.data;
 
+    api.defaults.headers.Authorization = `Bearer ${token}`;
+
     yield put(signInSuccess(token, user));
     history.push('/dashboard');
   } catch (err) {
@@ -57,7 +59,18 @@ export function* signUp({ payload }) {
   }
 }
 
+export function setToken({ payload }) {
+  if (!payload) return;
+
+  const { token } = payload.auth;
+
+  if (!token) {
+    api.defaults.headers.Authorization = `Bearer ${token}`;
+  }
+}
+
 export default all([
+  takeLatest('persist/REHYDRATE', setToken),
   takeLatest('@auth/SIGN_IN_REQUEST', signIn),
   takeLatest('@auth/SIGN_UP_REQUEST', signUp),
 ]);
