e73314b8f5b7252c8d717b70649f7520e5051f40
Cauê Kotarski <caue1017@hotmail.com>
2020-04-16 02:55:46 -0300

    Create new routes

--- restaurant-app/src/routes/index.js
+++ restaurant-app/src/routes/index.js
@@ -11,8 +11,12 @@ import Dashboard from '~/pages/Dashboard';
 import Restaurants from '~/pages/Restaurants';
 import CreateRestaurant from '~/pages/CreateRestaurant';
 
-import CreateCategory from '~/pages/CreateCategory';
 import Categories from '~/pages/Categories';
+import CreateCategory from '~/pages/CreateCategory';
+
+import Dishes from '~/pages/Dishes';
+import CreateDish from '~/pages/CreateDish';
+
 
 export default function Routes() {
   return (
@@ -28,6 +32,9 @@ export default function Routes() {
 
       <Route path="/categorias" exact component={Categories} isPrivate />
       <Route path="/criar-categoria" exact component={CreateCategory} isPrivate />
+
+      <Route path="/pratos/:category_id" exact component={Dishes} isPrivate />
+      <Route path="/criar-prato/:category_id" exact component={CreateDish} isPrivate />
 
       <Route path="/" component={() => <h1>Erro 404</h1>} />
     </Switch>
